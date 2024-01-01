90f800f1cbbb2d1f9871ea1f1b8827410810ac00
TaeHoon <31303655+xoasml@users.noreply.github.com>
2024-01-01 18:12:05 +0900

    add: counter module

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {configureStore} from '@reduxjs/toolkit'
+import rootReducer from "./modules";
+
+const store = configureStore({
+    reducer: rootReducer
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+    <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
